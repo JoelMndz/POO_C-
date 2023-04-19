bffaedfd6b54b8c03ac54b6c9d33ec182075f67f
JoelMndz <luisjo3lml@gmail.com>
2023-04-19 07:54:27 -0500

    Introduccion a clase y contructores

---
+++ IntroduccionClases/IntroduccionClases/Cliente.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroduccionClases;
+
+public class Cliente
+{
+    public string nombre;
+    public string email;
+    public string direccion;
+    public string telefono;
+    public int edad;
+
+    public Cliente()
+    {
+        nombre = "por defecto";
+    }
+
+    public Cliente(string nombre, string email, string direccion, string telefono, int edad)
+    {
+        this.edad = edad;
+        this.email = email;
+        this.nombre= nombre;
+        this.direccion =direccion;
+        this.telefono = telefono;
+    }
+}
---
+++ IntroduccionClases/IntroduccionClases/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using IntroduccionClases;
+
+Cliente cliente = new Cliente("Joel","joel@gmail.com","calle 13","29854989",23);
+string saludo = "Hola mundo";
+Console.WriteLine(cliente.nombre);
+Console.WriteLine(cliente.email);
+
