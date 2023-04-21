257e9916f915e652ec49bdc737e611a3a375956f
JoelMndz <luisjo3lml@gmail.com>
2023-04-21 07:29:11 -0500

    Metodos

--- IntroduccionClases/IntroduccionClases/Cliente.cs
+++ IntroduccionClases/IntroduccionClases/Cliente.cs
@@ -27,4 +27,35 @@ public class Cliente
         this.direccion =direccion;
         this.telefono = telefono;
     }
+
+    public string MotrarInformacion(bool mayusculas, bool mostrarEdad)
+    {
+        var informacion = ConcatenarInformacion(mostrarEdad);
+        if(mayusculas == true)
+            informacion = ConvertirAMayusculas(informacion);
+        else
+            informacion = ConvertirAMinusculas(informacion);
+
+        return informacion;
+    }
+
+    private string ConcatenarInformacion(bool mostrarEdad = false)
+    {
+        var informacion = $"{nombre} - {email}";
+
+        if (mostrarEdad)
+            informacion += $" - {edad}";
+
+        return informacion;
+    }
+
+    private string ConvertirAMinusculas(string texto)
+    {
+        return texto.ToLower();
+    }
+
+    private string ConvertirAMayusculas(string texto)
+    {
+        return texto.ToUpper();
+    }
 }
--- IntroduccionClases/IntroduccionClases/Program.cs
+++ IntroduccionClases/IntroduccionClases/Program.cs
@@ -3,7 +3,6 @@
 using IntroduccionClases;
 
 Cliente cliente = new Cliente("Joel","joel@gmail.com","calle 13","29854989",23);
-string saludo = "Hola mundo";
-Console.WriteLine(cliente.nombre);
-Console.WriteLine(cliente.email);
+
+Console.WriteLine(cliente.MotrarInformacion(false, true));
 
