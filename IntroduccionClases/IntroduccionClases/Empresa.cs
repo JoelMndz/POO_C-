acefbc0153ee57d6c393edfd19b4cbf445ccda30
JoelMndz <luisjo3lml@gmail.com>
2023-04-25 08:07:52 -0500

    propiedades y encapsulamiento

---
+++ IntroduccionClases/IntroduccionClases/Empresa.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroduccionClases;
+
+public class Empresa
+{
+	public string Codigo { get; } = "PIXXX123";
+	public string Nombre { get; set; } = string.Empty;
+	public string Email { get; set; } = string.Empty;
+	public string Direccion { get; set; } = string.Empty;
+	public string Telefono { get; set; } = string.Empty;
+	/*
+	private string nombre;
+
+	public string Nombre
+	{
+		get { return nombre; }
+		set { 
+			if(string.IsNullOrEmpty(value))
+				Console.WriteLine("Error, debes enviar el nombre");
+			else
+				nombre = value.ToUpper(); 
+		}
+	}*/
+
+}
